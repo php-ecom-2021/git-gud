368dbafc91660c89c39d4666a3936c5d34f73410
Martin Knudsen <mkk@webkenth.dk>
2021-10-07 13:15:45 +0200

    gitgud

--- public/index.php
+++ public/index.php
@@ -1,4 +1,32 @@
 <?php
 
 $content = file_get_contents('https://jsonplaceholder.typicode.com/posts');
-var_dump($content);
+$data = json_decode($content, true);
+var_dump($data);
+$id = array_map(function($item){
+    return $item['id'];
+}, $data);
+var_dump($id);
+
+
+$totals = [
+    'Tax',
+    'Freight & Handling',
+    'Subtotal',
+    'Grand total'
+];
+array_swap($Totals, 0,1);
+$first = time();
+
+sleep(10);
+var_dump(time() - $first);
+
+function array_swap(&$array,$swap_a,$swap_b) {
+    list(
+        $array[$swap_a],
+        $array[$swap_b]
+        ) = [
+        $array[$swap_b],
+        $array[$swap_a]
+    ];
+}
